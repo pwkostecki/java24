2c2321f11ad0c1515a9292e3ee6630ac313135fb
Piotr Kostecki <piotrwitoldkosteckI@gmail.com>
2024-08-27 11:34:06 -0600

    Add files via upload

---
+++ ImproperFractionToMixedNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ * In this app, the user will enter an improper fraction, and the app will convert it to a mixed number
+ * see the readme for some additional instructions.
+ *
+ * @author (write your name)
+ * @version (write a version number or a date)
+ */
+public class ImproperFractionToMixedNumber
+{
+   public static void main(String[] args)
+   {
+       Scanner keyboard = new Scanner(System.in);
+       
+       
+   }
+}
